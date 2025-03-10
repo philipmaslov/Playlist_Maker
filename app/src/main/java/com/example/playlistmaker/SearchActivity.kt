0c3ac8dc42097efa9403dfaa4cd2e59f46d93af0
YourFullName <fmaslov@beeline.ru>
2025-03-10 13:48:51 +0300

    Ссылка на урок: [Спринт 9/29 → Тема 3/5: EditText и TextWatcher → Урок 3/3: Домашнее задание] - добавил сохранение значения строки поиска

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -7,21 +7,20 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchValue = SEARCH_INPUT_DEF
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val inputEditText = findViewById<EditText>(R.id.input_edit_text)
         val clearButton = findViewById<ImageView>(R.id.clear_button)
@@ -31,17 +30,39 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 clearButton.visibility = clearButtonVisibility(p0)
+                searchValue = p0.toString()
             }
 
             override fun afterTextChanged(p0: Editable?) {
+
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        findViewById<EditText>(R.id.input_edit_text).setText(searchValue)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_INPUT, searchValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchValue = savedInstanceState.getString(SEARCH_INPUT, SEARCH_INPUT_DEF).toString()
+    }
+
+    companion object {
+        const val SEARCH_INPUT = "SEARCH_INPUT"
+        const val SEARCH_INPUT_DEF = ""
     }
 }
 
